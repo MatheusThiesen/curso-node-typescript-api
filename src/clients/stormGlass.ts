52a48547a14d7f3268aa77f9bfa4b214db745444
MatheusThiesen <matheusreis314@gmail.com>
2021-02-12 17:58:40 -0300

    [C02P01-2] - Desenvolvendo API client - Normalização de dados

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,36 @@
+import { AxiosStatic } from 'axios';
+
+export interface StormGlassPointSource {
+  [key: string]: number;
+}
+
+export interface StormGlassPoint {
+  time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {}
+
+export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPISource = 'noaa';
+
+  constructor(protected request: AxiosStatic) {}
+
+  public async fetchPoints(
+    lat: number,
+    lng: number
+  ): Promise<StormGlassForecastResponse> {
+    this.request.get(
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+    );
+    return Promise.resolve({});
+  }
+}
